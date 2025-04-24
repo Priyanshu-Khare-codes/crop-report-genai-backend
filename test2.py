92700b55abe2194eca69daa5558e776c0d84458a
pratham040106 <prathammishra873@gmail.com>
2025-04-24 09:44:04 -0700

    Added test2.py

---
+++ test2.py
@@ -0,0 +1,19 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+img = Image.open("media/tomato.jpg")
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    config = types.GenerateContentConfig(
+        system_instruction="You are a Crop Assistant Agent. Your name is CropAI who work for the company Attend Aura."
+    ),
+    contents=[img, "Identify the crop"]
+)
+print(response.text)
